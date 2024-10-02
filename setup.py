7966249b48a3828c1c52de4362adf1383b6940c7
cmdenthusiant <53386624+cmdenthusiant@users.noreply.github.com>
2024-10-03 00:02:57 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,4 @@
+import subprocess
+import sys
+
+subprocess.run([sys.executable, "-m", "pip", "install", "pywin32", "screeninfo", "opencv-python", "decord"])
--- wallpaper.py
+++ wallpaper.py
@@ -3,6 +3,8 @@ import win32gui
 import cv2
 import screeninfo
 import math
+import time
+import decord
 
 screen = screeninfo.get_monitors()[0]
 screenSize = (screen.width, screen.height)
@@ -26,19 +28,21 @@ wallpaper = win32gui.FindWindow(None, "wallpaperPY")
 cv2.setWindowProperty("wallpaperPY", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 win32gui.SetParent(wallpaper, worker)
 
-video = cv2.VideoCapture('video.mp4')
-fps = video.get(cv2.CAP_PROP_FPS)
-frameDelay = math.ceil(1000/fps)
-try:
-    while video.isOpened():
-        success, frame = video.read()
-        if success:
-            frame = cv2.resize(frame, screenSize, interpolation=cv2.INTER_LINEAR)
-            cv2.imshow("wallpaperPY", frame)
-            cv2.waitKey(1)
-            
-        else: video.set(cv2.CAP_PROP_POS_FRAMES, 0)
-except:
-        win32api.SendMessage(progman, 0x052C,  0x0000000D,  0)
-        win32api.SendMessage(progman, 0x052C,  0x0000000D,  1)
-        win32gui.EnumWindows(findWorker, 0x0000000D)
+vr = decord.VideoReader('video.mp4',width=screenSize[0], height=screenSize[1])
+
+framesLen = len(vr)
+fps = vr.get_avg_fps()
+max_FPS = 60
+stepframe = max(int(fps/max_FPS),1)
+estimFrameDelay = math.ceil(1000/fps)
+print(estimFrameDelay,fps)
+
+offsetFrameDelay = 0
+
+while True:
+    for i in range(0,framesLen,stepframe):
+        s = time.time()
+        cv2.imshow("wallpaperPY", cv2.cvtColor(vr[i].asnumpy(), cv2.COLOR_BGR2RGB))
+        cv2.waitKey(max(estimFrameDelay + offsetFrameDelay,1))
+        e = time.time()
+        offsetFrameDelay = estimFrameDelay - math.ceil((e-s)*1000)
